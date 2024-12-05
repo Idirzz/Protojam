e6f3d3521531c360f6532ee92f0e5af480784c7a
Julien-44100 <julienbotar44100@yahoo.com>
2024-12-05 17:30:42 +0100

    import componants citations.tsx image.tsx dans home.tsx

--- client/src/components/citation.tsx
+++ client/src/components/citation.tsx
@@ -1,14 +1,12 @@
 import "/src/citationcss/citation.css";
 // import Citation from "./components/citation";
 // import Image from "./components/image";
-export default function citation() {
+export default function Citation() {
   return (
     <body>
       <section className="citation">
         <p>salut</p>
       </section>
     </body>
-    //  <Image />
-    //  <Citation />
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,14 @@
+import Citation from "../components/citation.tsx";
+import Image from "../components/image.tsx";
+
 function Home() {
-  return <h1> Je suis dans Home</h1>;
+  return (
+    <>
+      <h1> Je suis dans Home</h1>
+      <Citation />
+      <Image />
+    </>
+  );
 }
 
 export default Home;
