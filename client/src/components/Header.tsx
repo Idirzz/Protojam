a66b87232ade1b0138cdf7129b9a75afd13fe3c3
Marcellin123 <marcellindikelel@yahoo.fr>
2024-12-05 23:18:30 +0100

    Header-available

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,17 @@
+import "../Header.css";
+import { Link } from "react-router-dom";
+function Header() {
+  return (
+    <header>
+      <div className="first-div">
+        <Link to="/destination">
+          <img className="profil" src="image/téléchargement.jpg" alt="Profil" />
+        </Link>
+        <p>Bonté à modérer</p>
+        <img className="logo" src="image/rounded_image_2.webp" alt="" />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
