86c2a166e8240dfca7d18a97c0a3326d81b9ea14
MellerinCharly <mellerincharly.pro@gmail.com>
2024-12-05 16:56:09 +0100

    construction of the card action to be carrie

---
+++ client/src/components/ActionToPerforms.tsx
@@ -0,0 +1,17 @@
+import "./ActionToPerforms.css"; // Gardons le même fichier de styles
+
+const Card = () => {
+  return (
+    <div className="main-card">
+      {/* Card principale avec texte lorem */}
+      <div className="card">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
+          vehicula dapibus magna, et tincidunt orci tincidunt eu.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
