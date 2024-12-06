87520e02f446187ea8ef929b91bea182a705b2a8
Julien-44100 <julienbotar44100@yahoo.com>
2024-12-06 11:20:50 +0100

    commit before git pull for receive yhe modification of all pepole

--- client/src/components/citation.tsx
+++ client/src/components/citation.tsx
@@ -2,12 +2,5 @@ import "/src/citationcss/citation.css";
 // import Citation from "./components/citation";
 // import Image from "./components/image";
 export default function Citation() {
-  return (
-    <p>
-      "Faites tout le bien que vous pouvez, par tous les moyens que vous pouvez,
-      de toutes les manières que vous pouvez, dans tous les endroits que vous
-      pouvez, à toutes les personnes que vous pouvez, aussi longtemps que vous
-      le pouvez"– John Wesley
-    </p>
-  );
+  return <></>;
 }
