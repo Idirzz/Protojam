b18ff5f77c16093ac38d26ec254fa8d824f68e6f
Julien-44100 <julienbotar44100@yahoo.com>
2024-12-06 10:14:39 +0100

    ajoute citation de John Wesley danzs citation.tsx

--- client/src/components/citation.tsx
+++ client/src/components/citation.tsx
@@ -5,7 +5,12 @@ export default function Citation() {
   return (
     <body>
       <section className="citation">
-        <p>salut</p>
+        <p>
+          "Faites tout le bien que vous pouvez, par tous les moyens que vous
+          pouvez, de toutes les manières que vous pouvez, dans tous les endroits
+          que vous pouvez, à toutes les personnes que vous pouvez, aussi
+          longtemps que vous le pouvez"– John Wesley
+        </p>
       </section>
     </body>
   );
